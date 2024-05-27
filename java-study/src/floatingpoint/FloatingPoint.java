972b22e8719832424e84ae34afded2ecca4b87e3
devjohnpark <devjohnpark@gmail.com>
2024-05-27 23:59:29 +0900

    Edit Operator Class

--- java-study/src/floatingpoint/FloatingPoint.java
+++ java-study/src/floatingpoint/FloatingPoint.java
@@ -44,40 +44,83 @@ public class FloatingPoint {
     }
 
     public static void main(String[] args) {
+//        float floatValue = -112.8125f;
+//        System.out.println(floatValue);
+//
+//        int intValue = Float.floatToIntBits(floatValue);
+//        String binaryString = Integer.toBinaryString(intValue);
+//        System.out.println("binaryString: " + binaryString);
+//        binaryString = String.format("%32s", binaryString).replace(' ', '0');
+//
+//        System.out.println("Float value: " + floatValue); // 1.4E-45
+//        System.out.println("Binary representation: " + binaryString); // 00000000000000000000000000000001
+//
+//        float unsignFloatMinValue = Float.MIN_VALUE; // 1.4E-45 (1.4e-45f)
+//        float unsignFloatMaxValue = Float.MAX_VALUE; // 3.4028235E38
+//        System.out.println("Unsign Float Min Value: " + unsignFloatMinValue);
+//        System.out.println("Unsign Float Max Value: " + unsignFloatMaxValue);
+//
+//        float signFloatMinValue = -Float.MIN_VALUE;
+//        float signFloatMaxValue = -Float.MAX_VALUE;
+//        System.out.println("Sign Float Min Value: " + signFloatMinValue);
+//        System.out.println("Sign Float Min Value: " + signFloatMaxValue);
+
+//        // 지수부 비트 추출
+//        float floatValue = -112.8125f;
+//        int exponentBits = Float.floatToIntBits(floatValue); // float 타입을 값을 int 타입의 값으로 해석하여 반환
+//        System.out.println("exponentBits " + exponentBits); // 0 01111110 01000000000000000000000 비트를 1063883029 정수로 출력
+////        System.out.println("fdf" + FloatingPoint.numToBits(-112.8125f));
+//        int exponent = (exponentBits >>> 23) & 0xFF; // 8비트가 모두 1로 이루어진 값인, 0xFF와 AND 연산하여 지수부 비트 추출
+//        System.out.println(floatValue + " 지수부: " + (exponent - 127)); // 0.1101에서 1.101 x 2^-1으로 표현하여, 지수는 -1이다.
+//
+//        // 가수부 비트 추출
+//        int mantissa = Float.floatToIntBits(exponentBits) & 0x7FFFFF; // 23비트가 모두 1로 이루어진 값인, 0x7FFFFF와 AND 연산하여 가수부 비트 추출
+//        String binaryMantissa = Integer.toBinaryString(mantissa); // 가수 부분을 이진수 문자열로 변환
+//
+//        // 가수 부분을 23비트로 만들기 위해 공백을 0으로 대체
+//        binaryMantissa = String.format("%23s", binaryMantissa).replace(' ', '0');
+//        System.out.println(floatValue + "가수부: " + binaryMantissa);
+
+//        float f31 = 123.4567f;
+//        System.out.println(numToExponentBits(f31));
+//        System.out.println(numToBits(f31));
+//        0.00011001
+//        00111101110011001100110011001101
+
+
+        /* 부동 소수점 전체 추출 */
+
         float floatValue = -112.8125f;
-        System.out.println(floatValue);
+        System.out.println("Decimal: " + floatValue); // -112.8125
 
-        int intValue = Float.floatToIntBits(floatValue);
-        String binaryString = Integer.toBinaryString(intValue);
-        System.out.println("binaryString: " + binaryString);
-        binaryString = String.format("%32s", binaryString).replace(' ', '0');
+        // int 타입 bit로 변환 (float와 int는 32bit로 동일한 크기)
+        int intBits = Float.floatToIntBits(floatValue);
 
-        System.out.println("Float value: " + floatValue); // 1.4E-45
-        System.out.println("Binary representation: " + binaryString); // 00000000000000000000000000000001
+        // 부동 소수점 전체 이진수 문자열 변환 후, 32비트 중 뒷자리수 비트 공백일시 0으로 교체
+        String binStr = String.format("%32s", Integer.toBinaryString(intBits)).replace(' ', '0');
+        System.out.println("Binary: " + binStr); // 11000010111000011010000000000000
 
-        float unsignFloatMinValue = Float.MIN_VALUE; // 1.4E-45 (1.4e-45f)
-        float unsignFloatMaxValue = Float.MAX_VALUE; // 3.4028235E38
-        System.out.println("Unsign Float Min Value: " + unsignFloatMinValue);
-        System.out.println("Unsign Float Max Value: " + unsignFloatMaxValue);
 
-        float signFloatMinValue = -Float.MIN_VALUE;
-        float signFloatMaxValue = -Float.MAX_VALUE;
-        System.out.println("Sign Float Min Value: " + signFloatMinValue);
-        System.out.println("Sign Float Min Value: " + signFloatMaxValue);
+        /* 지수부와 지수 추출 */
 
-        // 지수부 비트 추출
-        float f = 0.8125f;
-        int exponentBits = Float.floatToIntBits(f); // float 타입을 값을 int 타입의 값으로 해석하여 반환
-        System.out.println("exponentBits " + exponentBits); // 0 01111110 01000000000000000000000 비트를 1063883029 정수로 출력
-        int exponent = (exponentBits >>> 23) & 0xFF; // 8비트가 모두 1로 이루어진 값인, 0xFF와 AND 연산하여 지수부 비트 추출
-        System.out.println("Float.MAX_VALUE의 지수부: " + (exponent - 127)); // 0.1101에서 1.101 x 2^-1으로 표현하여, 지수는 -1이다.
+        // 가수부 자리수인 23비트 오른쪽 시프트 연산 자리수 버리고, 0xFF(8bit 모두 1)와 AND 연산하여 지수부 비트 추출
+        int exponent = (intBits >>> 23) & 0xFF; // 지수부
 
-        // 가수부 비트 추출
-        int mantissa = Float.floatToIntBits(Float.MAX_VALUE) & 0x7FFFFF; // 23비트가 모두 1로 이루어진 값인, 0x7FFFFF와 AND 연산하여 가수부 비트 추출
-        String binaryMantissa = Integer.toBinaryString(mantissa); // 가수 부분을 이진수 문자열로 변환
+        // 지수부 이진수 문자열 변환 후, 지수부 8비트 추출 및 공백 0으로 대체
+        String binExponent = String.format("%8s", Integer.toBinaryString(exponent)).replace(' ', '0');
+        System.out.println(floatValue + "의 지수부 " + binExponent);
 
-        // 가수 부분을 23비트로 만들기 위해 공백을 0으로 대체
-        binaryMantissa = String.format("%23s", binaryMantissa).replace(' ', '0');
-        System.out.println("Float.MAX_VALUE의 가수부: " + binaryMantissa);
+        // 지수부 = 지수 + bias 이기 때문에, float 타입의 bias 값 127을 뺀다.
+        System.out.println(floatValue + "의 지수: " + (exponent - 127)); // 지수
+
+
+        /* 가수부 비트 추출 */
+
+        // 0x7FFFFF(23bit 모두 1)와 AND 연산하여 가수부 비트 추출
+        int mantissa = intBits & 0x7FFFFF;
+
+        // 가수부 이진수 문자열 변환 후, 23비트로 만들기 위해 뒷자리 공백을 0으로 대체
+        String binMantissa = String.format("%23s", Integer.toBinaryString(mantissa)).replace(' ', '0');
+        System.out.println(floatValue + "의 가수부: " + binMantissa);
     }
 }
--- java-study/src/operator/Operator.java
+++ java-study/src/operator/Operator.java
@@ -4,15 +4,99 @@ package operator;
 // '+' 기호를 덧셈 연산자(Addition Opeator)라 한다.
 // 사칙 연산자는 +, -, *, / 등 다양한 연사자들이 있다.
 
+import integers.Integers;
+
+import java.util.Random;
+
 public class Operator {
     public static void main(String[] args) {
-        int n = 10;
-        int x = n + 10;
+//        int n = 10;
+//        int x = n + 10;
+//
+//        if (x == n + 10) {
+//            System.out.println("equal");
+//        } else {
+//            System.out.println("not equal");
+//        }
+//
+//        // 1101 = 13
+//        // 1001 = 9
+//        // 1001 = 9
+//        System.out.println(13 & 9);
 
-        if (x == n + 10) {
-            System.out.println("equal");
-        } else {
-            System.out.println("not equal");
-        }
+
+//        int x = 10;
+//        float y = 13.21f;
+//        float result = x + y; // 23.21
+//        System.out.println(result);
+
+
+        // ++: 수식 연산 후, --: 수식 연산 전
+        // --수식 연산 하기전에 그전 수식이 수행되야하므로 + 1 후에 - 1하여  결과값은 0
+//        System.out.println(x++ - --x);
+//        int x = 10;
+//        float y = 13.21f;
+//        int result = x + (int) y;
+////        System.out.println(result);
+////        int result = x + y;
+
+
+//        int x = 3;
+//        int y = 2;
+//        float result = x / (float) y ; // 1.5 (변수 x가 float으로 자동 타입 캐스팅 되어 연산)
+//        System.out.println(result);
+//
+//        int n = 3;
+//        int m = 0;
+//        if (n/m == 10) {
+//            System.out.println("sucess");
+//        }
+//        System.out.println(n/m);
+
+        byte x = 1;
+        byte y = 2;
+//        byte z = (byte) (x + y);
+        int z = x + y;
+        System.out.println(z);
+
+
+        char c1 = 'a';
+        char c2 = (char) (c1 + 1);
+        char c3 = 'a' + 1;
+        System.out.println(c3);
+
+
+        int i1 = 0xA; // 10(10) 1010(2)
+        int i2 = 0x3; //  3(10) 0011(2)
+        System.out.println(i1 | i2); // 11(10) 1011(2)
+        System.out.println(i1 & i2); // 2(10) 10(2)
+        System.out.println(i1 ^ i2); // 9(10) 1001(2)
+
+
+        Random random = new Random(); // Random 객체 생성
+        int publicKey = random.nextInt(100);  // 0부터 99까지의 정수 난수 생성
+        int encryptingData = 100;
+
+        int encryptedData = encryptingData ^ publicKey;
+        int decryptedData = encryptedData ^ publicKey;
+
+        System.out.printf("%d, %d\n", encryptingData, decryptedData);
+
+
+        // 11111111111111111111111111111011
+        int signedNum = -5; // -5를 나타내는 이진수는 11111011
+        int complementOfOne = ~signedNum;
+        System.out.println(complementOfOne);
+
+        int complementOfTwo = complementOfOne + 1;
+        System.out.println(complementOfTwo);
+
+
+        System.out.println();
+        System.out.println(Integers.numToBits(signedNum));
+        int shiftedResult = signedNum >>> 29; // 오른쪽으로 29비트 시프트
+        System.out.println(shiftedResult); // 결과 출력
+
+        System.out.println(Integer.toBinaryString(-5 & 0xFFFFFFF));
     }
 }
