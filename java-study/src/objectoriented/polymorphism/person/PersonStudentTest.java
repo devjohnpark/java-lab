b92658946868ab97eaf66811b8c8e2fb64b32f3b
devjohnpark <devjohnpark@gmail.com>
2024-06-21 18:02:41 +0900

    Add objectoriented.polymorphism.person package

--- java-study/src/objectoriented/polymorphism/person/PersonStudentTest.java
+++ java-study/src/objectoriented/polymorphism/person/PersonStudentTest.java
@@ -5,15 +5,13 @@ public class PersonStudentTest {
         Person p = new Student("Sujin", 21, "CS", 0);
         Student s = new Student("Minsu", 20, "CS", 1);
 
-//        p = s;
-//        s = (Student) p;
+        p = s; // 자식 타입 부모 타입으로 업 캐스팅 (타입 캐스팅 생략)
+        s = (Student) p; // 부모 타입 자식 타입으로 다운 캐스팅 (타입 캐스팅 생략 불가)
 
         Person person = new Person("Sujin", 21);
         Student student = new Student("Minsu", 20, "CS", 1);
 
-        // 타입 캐스팅 할 타입으로의 표현 범위가 모두 포함되면 생략가능
-//        person = s; // 자식 타입 부모 타입으로 업 캐스팅 (타입 캐스팅 생략)
-        student = (Student) person; // 부모 타입 자식 타입으로 다운 캐스팅 (타입 캐스팅 생략 불가)
+        student = (Student) person; // ClassCastException
 
 //        System.out.println(student.major);
 
